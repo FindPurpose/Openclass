fc485989f515fa776becd13f992782ce1739f480
FindPurpose <1009minpark@gmail.com>
2024-03-14 01:49:26 -0700

    fixing little things

--- load.py
+++ load.py
@@ -4,12 +4,12 @@ import pandas as pd
 data = extract.extract_books("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
 data_ = []
 data_.append(data.get_info()) 
-_data = pd.DataFrame(data_, columns = ["product_page_url", "book_title", "universal_ product_code (upc)", "price_including_tax", "price_excluding_tax", "tax", "quantity_available", "delete2", "product_description", "category", "review_rating", "image_url"])
+_data = pd.DataFrame(data_, columns = ["product_page_url", "book_title", "universal_ product_code (upc)", "price_including_tax", "price_excluding_tax", "tax", "quantity_available", "delete", "product_description", "category", "review_rating", "image_url"])
 #Maybe use this? columns = ["product_page_url", "book_title", "universal_ product_code (upc)", "price_including_tax", "price_excluding_tax", "tax", "quantity_available", "delete", "product_description", "category", "review_rating", "image_url"]
 """
 delete tax portion later
 """
 # _data = _data.drop("index",axis=1)
-_data = _data.drop("delete2", axis=1)
+_data = _data.drop("delete", axis=1)
 _data = _data.drop("tax", axis=1)
 _data = _data.to_csv("test.csv", index=False)
